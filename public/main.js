cf13fa5fcfbb1a293468c48ad5ef7bad7a5a6c45
Josh Mann <jlmann72@gmail.com>
2020-09-16 19:04:47 -0400

    Added and Tested TeamOnePlusClick Event

--- public/main.js
+++ public/main.js
@@ -5,12 +5,16 @@ function main() {
   // Query Selectors
   // Team 1 input querySelctor
   const teamOneNameInput = document.querySelector('.team1 input')
-  console.log(teamOneNameInput)
+  // console.log(teamOneNameInput)
   // Team 2 input querySelector
   const teamTwoNameInput = document.querySelector('.team2 input')
-  console.log(teamTwoNameInput)
-
+  // console.log(teamTwoNameInput)
+  // Team 1 plus icon querySelector
+  const teamOnePlusIcon = document.querySelector('.team1 .add')
+  // console.log(teamOnePlusIcon)
   // teamOneNameInput.value = 'Anything Else I Want'
+  let teamOneScore = 0
+  let teamTwoScore = 0
 
   // Team 1 name change event handler
   const handleChangeTeamOneName = event => {
@@ -28,9 +32,18 @@ function main() {
     teamNameElement.textContent = inputFieldValue
   }
 
+  // Team 1 Plus Icon click event handler
+  const handleTeamOnePlusClick = event => {
+    const target = event.target
+    const teamOneScoreString = document.querySelector('.team1 h3')
+    teamOneScore += 1
+    teamOneScoreString.textContent = teamOneScore.toString()
+  }
+
   // Event Listeners
   teamOneNameInput.addEventListener('input', handleChangeTeamOneName)
   teamTwoNameInput.addEventListener('input', handleChangeTeamTwoName)
+  teamOnePlusIcon.addEventListener('click', handleTeamOnePlusClick)
 }
 
 document.addEventListener('DOMContentLoaded', main)
