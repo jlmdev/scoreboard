ab6d601081dfe2c972998125876a2a879b86d4bf
Josh Mann <jlmann72@gmail.com>
2020-09-16 17:03:05 -0400

    Added Event Handlers for name chages

--- public/main.js
+++ public/main.js
@@ -2,6 +2,30 @@ function main() {
   if (document.querySelector('h1.hello-world')) {
     document.querySelector('h1.hello-world').textContent = 'Hello, World!'
   }
+  // Team 1 input querySelctor
+  const teamOneNameInput = document.querySelector('.team1 input')
+  console.log(teamOneNameInput)
+  // Team 2 input querySelector
+  const teamTwoNameInput = document.querySelector('.team2 input')
+  console.log(teamTwoNameInput)
+
+  // teamOneNameInput.value = 'Anything Else I Want'
+
+  // Team 1 name change event handler
+  const handleChangeTeamOneName = event => {
+    const elementThatChanged = event.target
+    const inputFieldValue = elementThatChanged.value
+    const teamNameElement = document.querySelector('.team1 h2')
+    teamNameElement.textContent = inputFieldValue
+  }
+
+  // Team 2 name change event handler
+  const handleChangeTeamTwoName = event => {
+    const elementThatChanged = event.target
+    const inputFieldValue = elementThatChanged.value
+    const teamNameElement = document.querySelector('.team2 h2')
+    teamNameElement.textContent = inputFieldValue
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main)
