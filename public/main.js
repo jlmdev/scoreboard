d42e17c1840b404af4887c677eb1e1ad6d54bb13
Josh Mann <jlmann72@gmail.com>
2020-09-16 19:22:50 -0400

    All Part 1 functions working

--- public/main.js
+++ public/main.js
@@ -14,6 +14,11 @@ function main() {
   // console.log(teamOnePlusIcon)
   // Team 2 plus icon querySelector
   const teamTwoPlusIcon = document.querySelector('.team2 .add')
+  // Team 1 minus icon querySelector
+  const teamOneMinusIcon = document.querySelector('.team1 .subtract')
+  // console.log(teamOneMinusIcon)
+  // Team 2 minus icon querySelector
+  const teamTwoMinusIcon = document.querySelector('.team2 .subtract')
 
   // variables
   let teamOneScore = 0
@@ -51,11 +56,29 @@ function main() {
     teamTwoScoreString.textContent = teamTwoScore.toString()
   }
 
+  // Team 1 Minus Icon click event handler
+  const handleTeamOneMinusClick = event => {
+    const target = event.target
+    const teamOneScoreString = document.querySelector('.team1 h3')
+    teamOneScore -= 1
+    teamOneScoreString.textContent = teamOneScore.toString()
+  }
+
+  // Team 2 Minus Icon click event handler
+  const handleTeamTwoMinusClick = event => {
+    const target = event.target
+    const teamTwoScoreString = document.querySelector('.team2 h3')
+    teamTwoScore -= 1
+    teamTwoScoreString.textContent = teamTwoScore.toString()
+  }
+
   // Event Listeners
   teamOneNameInput.addEventListener('input', handleChangeTeamOneName)
   teamTwoNameInput.addEventListener('input', handleChangeTeamTwoName)
   teamOnePlusIcon.addEventListener('click', handleTeamOnePlusClick)
   teamTwoPlusIcon.addEventListener('click', handleTeamTwoPlusClick)
+  teamOneMinusIcon.addEventListener('click', handleTeamOneMinusClick)
+  teamTwoMinusIcon.addEventListener('click', handleTeamTwoMinusClick)
 }
 
 document.addEventListener('DOMContentLoaded', main)
